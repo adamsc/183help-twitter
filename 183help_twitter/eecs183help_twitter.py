1e14379cef2bf1080ad94b9b292c6422f345e197
Adam Schnizer <adamschnitzer@gmail.com>
2014-04-13 18:48:09 -0400

    Added tweet responses

--- 183help_twitter/eecs183help_twitter.py
+++ 183help_twitter/eecs183help_twitter.py
@@ -2,13 +2,46 @@ from tweepy import *
 import ConfigParser
 import json
 import pprint
-
+import random
+import string
 
 def get_reply(text):
     """
     Returns a reply to the message text.
     """
-    return 'this is my reply!'
+    words = [w.strip(string.punctuation) for w in text.lower().split()]
+
+    general_responses = ['try asking on Piazza!',
+            'I recommend you go to office hours #NotMyProblem',
+            'did you try googling that?',
+            "I'm not sure! ask Maxim!",
+            'Try re-reading the lecture slides',
+            'This was covered in lecture',
+            'Search Piazza!',
+            'We will cover that in class later..',
+        ]
+
+    if 'autograder' in words:
+        return random.choice(['uh oh.. I think we broke it..',
+            "it's not a bug! just an undocumented feature!",
+        ] + general_responses)
+    elif 'extension' in words:
+        return random.choice(['yeah! you can get an extension.. when hell freezes over!! #SorryNotSorry',
+            ])
+    elif 'office' in words and 'hours' in words:
+        return random.choice(['current wait time: 2 weeks 5 days #subtweet #goaway',
+            ] + general_responses)
+    elif 'codelab' in words:
+        return random.choice(['we have no clue what it means either!',
+            ] + general_responses)
+    elif 'style' in words:
+        return random.choice(['ask Eva!!',
+            "don't at me! I only got 5/10 for style!",
+            "it's a magic number",
+            "this code is unreadable!",
+        ] + general_responses)
+    else:
+        return random.choice(general_responses)
 
 def read_config():
     """
